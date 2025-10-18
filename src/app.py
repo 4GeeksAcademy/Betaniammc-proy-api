c8aeec02992d4d02a4a4f44a1c1195cf3d3f4d9d
Betaniammc <126913973+Betaniammc@users.noreply.github.com>
2025-10-18 09:38:05 +0000

    proyecto final

--- src/app.py
+++ src/app.py
@@ -2,9 +2,32 @@ import os
 import pandas as pd
 import matplotlib.pyplot as plt
 from dotenv import load_dotenv
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+
 
 # load the .env file variables
 load_dotenv()
+client_id = os.environ.get("CLIENT_ID")
+client_secret = os.environ.get("CLIENT_SECRET")
+
+auth_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
+spotify = spotipy.Spotify(auth_manager=auth_manager)
+
+artist_id = "3TVXtAsR1Inumwj472S9r4"
+
+# Get the top tracks of an artist
+results = spotify.artist_top_tracks(artist_id)
+
+songs = []
+for track in results['tracks']:
+    songs.append({
+        'name': track['name'],
+        'popularity': track['popularity'],
+        'duration_min': track['duration_ms'] / 60000
+    })
+print(songs)
 
 
 # Spotify API credentials
